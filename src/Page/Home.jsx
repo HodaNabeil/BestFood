d1f1661a44f1d702aaa6bcf0c628d695296ce809
Hoda Nabeil <hodanabeil67@gmail.com>
2023-12-08 18:03:36 +0200

    Speed adjustment

--- src/Page/Home.jsx
+++ src/Page/Home.jsx
@@ -1,35 +1,20 @@
 
-import React, { Suspense } from "react"
 import Category from "../components/Category"
+import FilterMenu from "../components/FilterMenu"
 import Footer from "../components/Footer"
 import Header from "../components/Header"
-// import FilterMenu from "../components/FilterMenu"
-// import Hero from "../components/Hero"
-// import NewProduct from "../components/NewProduct"
-
-
+import Hero from "../components/Hero"
+import NewProduct from "../components/NewProduct"
 import ScrollTopButton from "../components/ScrollTopButton"
 
-const LazyFilterMenu = React.lazy(() => import('../components/FilterMenu'))
-
-const LazyNewProduct =React.lazy(()=> import ("../components/NewProduct"));
-
-
-const LazyHero = React.lazy(()=> import('../components/Hero'));
 
 function Home() {
   return (
     <div className=" overflow-hidden">
       <Header/>
-      <Suspense fallback='landing Page '>
-        <LazyHero/>
-      </Suspense>
-      <Suspense  fallback=" newprodcut ">
-        <LazyNewProduct/>
-      </Suspense>
-      <Suspense fallback= " Filter Menu">
-        <LazyFilterMenu/>
-      </Suspense>
+      <Hero/>
+      <NewProduct/>
+      <FilterMenu/>
       <Category/>
       <Footer/>
       <ScrollTopButton/>
--- src/index.js
+++ src/index.js
@@ -1,88 +1,98 @@
-import React, { Suspense } from "react";
-import ReactDOM from "react-dom/client";
-import "./index.css";
+import React, { Suspense } from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
 
-import reportWebVitals from "./reportWebVitals";
+import reportWebVitals from './reportWebVitals';
 
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import { Provider } from "react-redux";
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import { Provider } from 'react-redux'
+
+
 
 // import Home from './Page/Home';
 // import Services from './Page/Services';
-import Header from "./components/Header";
-import { store } from "./rtk/Store";
+import Header from './components/Header';
+import { store } from './rtk/Store';
 // import About from './Page/About';
-// import MenuOrder from './Page/MenuOrder';
+import MenuOrder from './Page/MenuOrder';
 
-const LazyServices = React.lazy(() => import("./Page/Services"));
+const LazyServices = React.lazy(() =>import('./Page/Services'));
 
-const LazyHome = React.lazy(() => import("./Page/Home"));
+const LazyHome = React.lazy(()=> import("./Page/Home"));
 
-const LazyAbout = React.lazy(() => import("./Page/About"));
+const LazyAbout = React.lazy(()=> import("./Page/About"));
 
-const LazyMenuOrder = React.lazy(() => import("./Page/MenuOrder"));
+
+
+
+
 
 const router = createBrowserRouter([
+
   {
     path: "/",
     element: (
       <div>
-        <Suspense fallback="Loading Page Home ">
-          <LazyHome />
-        </Suspense>
+      <Suspense fallback = "Loading Page Home ">
+        <LazyHome/>
+      </Suspense>
+        
       </div>
     ),
   },
   {
     path: "/about",
-    element: (
-      <div>
-        <Header />
-        <Suspense fallbac="Loading Page About">
-          <LazyAbout />
-        </Suspense>
-      </div>
-    ),
-  },
+    element: <div>
+      <Header/>
+      <Suspense fallbac= 'Loading Page About'>
+        <LazyAbout/>
+      </Suspense>
+      </div>,
+  }
+  ,
   {
     path: "/services",
-    element: (
-      <div>
-        <Header />
-        <React.Suspense fallback="Loading Page Serviecs ">
-          <LazyServices />
-        </React.Suspense>
-      </div>
-    ),
+    element: <div>
+        <Header/>
+          <React.Suspense  fallback= 'Loading ..'>
+          <LazyServices/>
+          </React.Suspense>
+        
+      </div>,
   },
   {
-    path: "/home",
-    element: (
-      <div>
-        <Suspense fallback="Loading Page Home ">
-          <LazyHome />
-        </Suspense>
+    path : '/contect',
+    element : <div>
+        <Header/>
+        <MenuOrder/>
       </div>
-    ),
-  },
+  }
+,
   {
-    path: "/menuorder",
-    element: (
-      <>
-        <Header />
-        <Suspense fallback="Loading Page Order">
-          <LazyMenuOrder />
-        </Suspense>
-      </>
-    ),
-  },
+    path : '/home',
+    element : <div>
+      <Suspense fallback = "Loading Page Home ">
+        <LazyHome/>
+      </Suspense>
+      </div>
+  },{
+    path: '/menuorder',
+    element:<>
+    <Header/>
+    <MenuOrder/>
+    </>
+  }
 ]);
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <RouterProvider router={router} />
+    <Provider store={store} >
+    <RouterProvider router={router} />
     </Provider>
+
   </React.StrictMode>
 );
 
